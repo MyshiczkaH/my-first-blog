069922c50af684ea1347d469c70901fead94c67e
Hanka Střondalová <strondalovah@centrum.cz>
2018-01-20 16:04:02 +0100

    Adding of all changes

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+#blog/urls.py
+
+from django.conf.urls import url
+from . import views #tecka je aktualni adresar
+
+urlpatterns = [ url ('^$', views.post_list, name='post_list'),]
+#striska dolar je regularni vyraz, moznost jak popsat retezce
+#jenom acka 'a*' nebo prazdny ' ' 'aaaa' 'aaaaaaa'
+#striska zacatek retezce a konec dolr.. takze zacatek konec o sobe takze nic :)
