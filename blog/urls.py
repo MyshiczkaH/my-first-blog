2a3a4c8c82d6f38c8bf89830b911ce8d8a986b2c
Hanka Střondalová <strondalovah@centrum.cz>
2018-01-20 17:59:12 +0100

    Last commit of this lesson
    blog/templates/blog/base.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,11 @@
 from django.conf.urls import url
 from . import views #tecka je aktualni adresar
 
-urlpatterns = [ url ('^$', views.post_list, name='post_list'),]
+urlpatterns = [ url ('^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)$', views.post_detail, name='post_detail'),
+    url('^post/new$', views.post_new, name='post_new'),
+
+]
 #striska dolar je regularni vyraz, moznost jak popsat retezce
 #jenom acka 'a*' nebo prazdny ' ' 'aaaa' 'aaaaaaa'
 #striska zacatek retezce a konec dolr.. takze zacatek konec o sobe takze nic :)
